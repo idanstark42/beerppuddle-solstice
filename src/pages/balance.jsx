545c44bae09d1d4db227674d90a550fb9894f901
Idan Stark <idanstark42@gmail.com>
2024-12-13 22:37:26 +0200

    cosmetic fixes

--- src/pages/balance.jsx
+++ src/pages/balance.jsx
@@ -1,8 +1,29 @@
-
+import { useState, useEffect } from "react"
 import { useData } from "../logic/data-access"
+
+const MAX_GRADIENT = 10
+const MIN_GRADIENT = 0
+
+const SPINNING_SPEED = 10
+const BALANCE_SPEED = 10
 
 export default function Balance () {
   const { data } = useData()
+  const [angle, setAngle] = useState(90)
+  const [gradient, setGradient] = useState({ value: MIN_GRADIENT, growing: true })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setAngle((prev) => (prev + SPINNING_SPEED / 10) % 360)
+      // setGradient((prev) => {
+      //   const { value, growing } = prev
+      //   const newGrowing = value >= MAX_GRADIENT ? false : value <= MIN_GRADIENT ? true : growing
+      //   return { value: value + (newGrowing ? 1 : -1) * BALANCE_SPEED / 10, growing: newGrowing }
+      // })
+    }, 100)
+
+    return () => clearInterval(interval)
+  }, [])
 
   if (data.length === 0) {
     return <div>Loading...</div>
@@ -11,14 +32,11 @@ export default function Balance () {
   const whites = data.filter(player => player.color === 'white').length
   const blacks =  data.length - whites
 
-  const white = whites / (whites + blacks)
   const black = blacks / (whites + blacks)
 
-  console.log('white', white)
-  console.log('black', black)
-
-  return <div className="balance">
-    <div className="balance-white" style={{ height: '100%', width: `${white * 100}%`, backgroundColor: 'white', position: 'absolute', top: 0, left: 0 }}></div>
-    <div className="balance-black" style={{ height: '100%', width: `${black * 100}%`, backgroundColor: 'black', position: 'absolute', top: 0, right: 0 }}></div>
-  </div>
+  return <div className="balance"
+    style={{
+      background: `linear-gradient(${angle}deg, black 0%, black ${100 * black - gradient.value}%, white ${100 * black + gradient.value}%, white 100%)`
+    }}
+  />
 }
